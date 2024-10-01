a4a7a1afbcef9f2c74d0b1e24e0e31846270632a
wnsrl <wnsrl981228@gmail.com>
2024-10-01 12:28:38 +0900

    first commit

---
+++ src/main/java/com/linkwiki/LinkwikiApplication.java
@@ -0,0 +1,13 @@
+package com.linkwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkwikiApplication.class, args);
+    }
+
+}
